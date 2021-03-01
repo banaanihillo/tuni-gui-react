a8a3898a0ff66a605f8d60a17753616d64a3f851
vjhmur@utu.fi <vjhmur@utu.fi>
2021-03-01 14:43:01 +0200

    Added exercise-1-1-small
    Exercise 1.1, small:
    Added three text input fields,
    for first, middle, and last name,
    with the middle name disabled by default,
    to be enabled via radio button,
    also added an auto-fill button,
    to populate the fields with bananas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [firstName, setFirstName] = useState("")
+    const [middleName, setMiddleName] = useState("")
+    const [middleNameEnabled, enableMiddleName] = useState(false)
+    const [lastName, setLastName] = useState("")
+    return (
+        <div>
+            <label htmlFor="first-name-input"> First name: </label>
+            <input
+                id="first-name-input"
+                value={firstName}
+                onChange={({target}) => setFirstName(target.value)}
+            />
+            <br />
+            <label htmlFor="middle-name-input"> Middle name: </label>
+            <input
+                id="middle-name-input"
+                value={middleName}
+                disabled={!middleNameEnabled}
+                onChange={({target}) => setMiddleName(target.value)}
+            />
+            <br />
+            <label htmlFor="middle-name-hidden"> Middle name </label>
+            {/*
+            The assignment mentions a checkbox,
+            but the visual cue has a radio button instead -
+            could alternatively use a checkbox here,
+            to enable it to be unchecked after initial selection,
+            but decided to just go with a radio input
+            */}
+            <input
+                type="radio"
+                id="middle-name-hidden"
+                value={middleNameEnabled}
+                onChange={() => enableMiddleName(true)}
+            />
+            <br />
+            <label htmlFor="last-name-input"> Last name: </label>
+            <input
+                id="last-name-input"
+                value={lastName}
+                onChange={({target}) => setLastName(target.value)}
+            />
+            <br />
+            <button
+                onClick={() => {
+                    setFirstName("Banaani")
+                    setMiddleName("banaanihillo")
+                    setLastName("Hillo")
+                }}
+            >
+                Auto-fill
+            </button>
+        </div>
+    )
 }
 
-export default App;
+export default App
