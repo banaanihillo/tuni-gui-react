4be67950e1f5aed2e68169c39603c87febf58cbe
vjhmur@utu.fi <vjhmur@utu.fi>
2021-03-01 17:59:05 +0200

    Added assignment 1-3-small (Mouse events)
    Assignment 1-3-small:
    Added three sections for mouse events,
    one for mouse buttons,
    one for mouse coordinate information,
    and one for mouse scroll wheel

--- src/App.js
+++ src/App.js
@@ -2,81 +2,27 @@ import React, {useState} from "react"
 import "./App.css"
 
 const App = () => {
-    const [bitSeven, toggleBitSeven] = useState(0)
-    const [bitSix, toggleBitSix] = useState(0)
-    const [bitFive, toggleBitFive] = useState(0)
-    const [bitFour, toggleBitFour] = useState(0)
-    const [bitThree, toggleBitThree] = useState(0)
-    const [bitTwo, toggleBitTwo] = useState(0)
-    const [bitOne, toggleBitOne] = useState(0)
-    const [bitZero, toggleBitZero] = useState(0)
-    const bits = String(bitSeven)
-        + String(bitSix)
-        + String(bitFive)
-        + String(bitFour)
-        + String(bitThree)
-        + String(bitTwo)
-        + String(bitOne)
-        + String(bitZero)
-    const [decimal, setDecimal] = useState(0)
-
+    const [mouseButton, setMouseButton] = useState("")
+    const [mouseInformation, setMouseInformation] = useState("")
+    const [wheelInformation, setWheelInformation] = useState("")
     return (
         <main>
-            <section className="checkbox-container">
-                <input
-                    type="checkbox"
-                    value={bitSeven}
-                    onChange={() => toggleBitSeven(bitSeven ? 0 : 1)}
-                />
-                <input
-                    type="checkbox"
-                    value={bitSix}
-                    onChange={() => toggleBitSix(bitSix ? 0 : 1)}
-                />
-                <input
-                    type="checkbox"
-                    value={bitFive}
-                    onChange={() => toggleBitFive(bitFive ? 0 : 1)}
-                />
-                <input
-                    type="checkbox"
-                    value={bitFour}
-                    onChange={() => toggleBitFour(bitFour ? 0 : 1)}
-                />
-                <input
-                    type="checkbox"
-                    value={bitThree}
-                    onChange={() => toggleBitThree(bitThree ? 0 : 1)}
-                />
-
-                <input
-                    type="checkbox"
-                    value={bitTwo}
-                    onChange={() => toggleBitTwo(bitTwo ? 0 : 1)}
-                />
-
-                <input
-                    type="checkbox"
-                    value={bitOne}
-                    onChange={() => toggleBitOne(bitOne ? 0 : 1)}
-                />
-
-                <input
-                    type="checkbox"
-                    value={bitZero}
-                    onChange={() => toggleBitZero(bitZero ? 0 : 1)}
-                />
+            <section onMouseDown={(event) => {
+                setMouseButton(`Mouse button ${event.button} clicked`)
+            }}>
+                {mouseButton || "Click me"}
             </section>
-            <section className="number-container">
-                <p>
-                    {bits}                
-                </p>
-                <button onClick={() => setDecimal(parseInt(bits, 2))}>
-                    Convert
-                </button>
-                <p>
-                    {decimal}
-                </p>
+            <section onMouseMove={(event) => {
+                setMouseInformation(
+                    `(${event.clientX}, ${event.clientY})`
+                )
+            }}>
+                {mouseInformation || "Hover on me"}
+            </section>
+            <section onWheel={(event) => {
+                setWheelInformation("Wheel delta Y: " + event.deltaY)
+            }}>
+                {wheelInformation || "Scroll over me"}
             </section>
         </main>
     )
