408c508c84a18782a9fbdeab4bf335f4e94d8950
vjhmur@utu.fi <vjhmur@utu.fi>
2021-03-01 17:12:51 +0200

    Added exercise-1-2-small (Conversion)
    Exercise 1.2-small:
    Added eight checkbox inputs,
    corresponding to a binary sequence,
    and a button to convert binary to decimal

--- src/App.js
+++ src/App.js
@@ -2,59 +2,83 @@ import React, {useState} from "react"
 import "./App.css"
 
 const App = () => {
-    const [firstName, setFirstName] = useState("")
-    const [middleName, setMiddleName] = useState("")
-    const [middleNameEnabled, enableMiddleName] = useState(false)
-    const [lastName, setLastName] = useState("")
+    const [bitSeven, toggleBitSeven] = useState(0)
+    const [bitSix, toggleBitSix] = useState(0)
+    const [bitFive, toggleBitFive] = useState(0)
+    const [bitFour, toggleBitFour] = useState(0)
+    const [bitThree, toggleBitThree] = useState(0)
+    const [bitTwo, toggleBitTwo] = useState(0)
+    const [bitOne, toggleBitOne] = useState(0)
+    const [bitZero, toggleBitZero] = useState(0)
+    const bits = String(bitSeven)
+        + String(bitSix)
+        + String(bitFive)
+        + String(bitFour)
+        + String(bitThree)
+        + String(bitTwo)
+        + String(bitOne)
+        + String(bitZero)
+    const [decimal, setDecimal] = useState(0)
+
     return (
-        <div>
-            <label htmlFor="first-name-input"> First name: </label>
-            <input
-                id="first-name-input"
-                value={firstName}
-                onChange={({target}) => setFirstName(target.value)}
-            />
-            <br />
-            <label htmlFor="middle-name-input"> Middle name: </label>
-            <input
-                id="middle-name-input"
-                value={middleName}
-                disabled={!middleNameEnabled}
-                onChange={({target}) => setMiddleName(target.value)}
-            />
-            <br />
-            <label htmlFor="middle-name-hidden"> Middle name </label>
-            {/*
-            The assignment mentions a checkbox,
-            but the visual cue has a radio button instead -
-            could alternatively use a checkbox here,
-            to enable it to be unchecked after initial selection,
-            but decided to just go with a radio input
-            */}
-            <input
-                type="radio"
-                id="middle-name-hidden"
-                value={middleNameEnabled}
-                onChange={() => enableMiddleName(true)}
-            />
-            <br />
-            <label htmlFor="last-name-input"> Last name: </label>
-            <input
-                id="last-name-input"
-                value={lastName}
-                onChange={({target}) => setLastName(target.value)}
-            />
-            <br />
-            <button
-                onClick={() => {
-                    setFirstName("Banaani")
-                    setMiddleName("banaanihillo")
-                    setLastName("Hillo")
-                }}
-            >
-                Auto-fill
-            </button>
-        </div>
+        <main>
+            <section className="checkbox-container">
+                <input
+                    type="checkbox"
+                    value={bitSeven}
+                    onChange={() => toggleBitSeven(bitSeven ? 0 : 1)}
+                />
+                <input
+                    type="checkbox"
+                    value={bitSix}
+                    onChange={() => toggleBitSix(bitSix ? 0 : 1)}
+                />
+                <input
+                    type="checkbox"
+                    value={bitFive}
+                    onChange={() => toggleBitFive(bitFive ? 0 : 1)}
+                />
+                <input
+                    type="checkbox"
+                    value={bitFour}
+                    onChange={() => toggleBitFour(bitFour ? 0 : 1)}
+                />
+                <input
+                    type="checkbox"
+                    value={bitThree}
+                    onChange={() => toggleBitThree(bitThree ? 0 : 1)}
+                />
+
+                <input
+                    type="checkbox"
+                    value={bitTwo}
+                    onChange={() => toggleBitTwo(bitTwo ? 0 : 1)}
+                />
+
+                <input
+                    type="checkbox"
+                    value={bitOne}
+                    onChange={() => toggleBitOne(bitOne ? 0 : 1)}
+                />
+
+                <input
+                    type="checkbox"
+                    value={bitZero}
+                    onChange={() => toggleBitZero(bitZero ? 0 : 1)}
+                />
+            </section>
+            <section className="number-container">
+                <p>
+                    {bits}                
+                </p>
+                <button onClick={() => setDecimal(parseInt(bits, 2))}>
+                    Convert
+                </button>
+                <p>
+                    {decimal}
+                </p>
+            </section>
+        </main>
     )
 }
 
