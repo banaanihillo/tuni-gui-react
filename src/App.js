264bdc5e5a154caf4abf56816cc3808f2ffbb56a
vjhmur@utu.fi <vjhmur@utu.fi>
2021-03-02 13:51:57 +0200

    Added the exercise 1-4-large layout
    Exercise 1-4-large:
    Added a grid for the checkbox input,
    along with some semi-functional labels,
    due to the table-like nature of the illustration,
    also added a canvas within a figure,
    alongside a figure caption,
    to act as a mouse event playground

--- src/App.js
+++ src/App.js
@@ -1,29 +1,65 @@
-import React, {useState} from "react"
+import React from "react"
 import "./App.css"
 
 const App = () => {
-    const [mouseButton, setMouseButton] = useState("")
-    const [mouseInformation, setMouseInformation] = useState("")
-    const [wheelInformation, setWheelInformation] = useState("")
+
     return (
         <main>
-            <section onMouseDown={(event) => {
-                setMouseButton(`Mouse button ${event.button} clicked`)
-            }}>
-                {mouseButton || "Click me"}
+            <section className="mouse-container">
+                <article className="mouse-checkbox-container">
+                    {/*
+                    The labels in the example look kinda funky,
+                    almost makes one think of a table,
+                    which is not really the most preferable way -
+                    it would be better in terms of user experience,
+                    to instead give each checkbox a unique label,
+                    for easier access to the input;
+                    screen readers follow them consistently,
+                    and mouse/touch users have a larger area to target
+                    */}
+                    <label className="grid-label">
+                        <b> Events </b>
+                    </label>
+
+                    <label> Mouse </label>
+                    <label> Scroll wheel </label>
+                    <label htmlFor="mouse-distance"> Distance </label>
+
+                    <input
+                        type="checkbox"
+                        id="mouse-distance"
+                    />
+                    <input
+                        type="checkbox"
+                        id="scroll-wheel-distance"
+                    />
+
+                    <label htmlFor="mouse-speed"> Speed </label>
+                    <input
+                        type="checkbox"
+                        id="mouse-speed"
+                    />
+                    <input
+                        type="checkbox"
+                        id="scroll-wheel-speed"
+                    />
+                </article>
+                <article className="mouse-information-container">
+                    <p>
+                        Check/uncheck the checkboxes,
+                        and see how the figure reacts.
+                    </p>
+                    <button>
+                        Reset distances
+                    </button>
+                </article>
             </section>
-            <section onMouseMove={(event) => {
-                setMouseInformation(
-                    `(${event.clientX}, ${event.clientY})`
-                )
-            }}>
-                {mouseInformation || "Hover on me"}
-            </section>
-            <section onWheel={(event) => {
-                setWheelInformation("Wheel delta Y: " + event.deltaY)
-            }}>
-                {wheelInformation || "Scroll over me"}
-            </section>
+            <figure className="mouse-event-figure">
+                <canvas className="mouse-event-canvas"></canvas>
+                <figcaption>
+                    See mouse events above
+                </figcaption>
+            </figure>
         </main>
     )
 }
