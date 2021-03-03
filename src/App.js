b84e3d8e01abbd2e72814026c81a0915ef085036
vjhmur@utu.fi <vjhmur@utu.fi>
2021-03-03 13:55:00 +0200

    Added exercise 1-4-large (Mouse events)
    Exercise 1-4, large:
    Added mouse-move and wheel events,
    bound to the canvas element,
    and toggled on/off via the checkbox input,
    also added an eternal timeout stopwatch,
    to calculate velocity based on the distances,
    and a reset button to reset time to 1,
    to avoid division by zero,
    and everything else to zero

--- src/App.js
+++ src/App.js
@@ -1,7 +1,92 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import "./App.css"
 
 const App = () => {
+    const [mouseDistanceEnabled, toggleMouseDistance] = useState(false)
+    const [
+        scrollWheelDistanceEnabled,
+        toggleScrollWheelDistance
+    ] = useState(false)
+    const [mouseSpeedEnabled, toggleMouseSpeed] = useState(false)
+    const [
+        scrollWheelSpeedEnabled,
+        toggleScrollWheelSpeed
+    ] = useState(false)
+    const [mouseDistance, setMouseDistance] = useState({
+        x: 0,
+        y: 0
+    })
+    const [scrollWheelDistance, setScrollWheelDistance] = useState(0)
+    const [mouseSpeed, setMouseSpeed] = useState(0)
+    const [scrollWheelSpeed, setScrollWheelSpeed] = useState(0)
+    const [
+        previousCoordinates,
+        setPreviousCoordinates
+    ] = useState(null)
+    const [timeElapsed, setTimeElapsed] = useState(1)
+    const [timerOn, toggleTimer] = useState(true)
+
+    useEffect(() => {
+        if (timerOn) {
+            const timeoutIdentifier = setTimeout(() => {
+                setTimeElapsed(timeElapsed + 1)
+            }, 1000)
+            return () => {
+                clearTimeout(timeoutIdentifier)
+            }
+        }
+    },
+    [timeElapsed, timerOn])
+
+    const onMouseMove = (event) => {
+        toggleTimer(true)
+        if (previousCoordinates) {
+            setMouseDistance({
+                x: mouseDistance.x + Math.abs(
+                    previousCoordinates.x
+                    - event.clientX
+                ),
+                y: mouseDistance.y + Math.abs(
+                    previousCoordinates.y
+                    - event.clientY
+                )
+            })
+            setMouseSpeed(Math.floor(
+                (mouseDistance.x + mouseDistance.y)
+                / timeElapsed
+            ))
+        }
+        setPreviousCoordinates({
+            x: event.clientX,
+            y: event.clientY
+        })
+    }
+
+    const onWheel = (event) => {
+        setScrollWheelDistance((prevState) => {
+            return (prevState + Math.abs(event.deltaY))
+        })
+        setScrollWheelSpeed(Math.floor(
+            scrollWheelDistance / timeElapsed
+        ))
+    }
+
+    /*
+    The specification separates the listeners into four,
+    however, it does seem much more logical to group them together,
+    since velocity is directly related to distance,
+    essentially leading to two listeners - mousemove and wheel,
+    which are removed only when both speed and distance are disabled
+    */
+    const toggleListeners = (() => {
+        const canvasElement = document.querySelector("canvas")
+        if (!mouseDistanceEnabled && !mouseSpeedEnabled) {
+            canvasElement.removeEventListener("mousemove", onMouseMove)
+        }
+        if (!scrollWheelDistanceEnabled && !scrollWheelSpeedEnabled) {
+            canvasElement.removeEventListener("wheel", onWheel)
+        }
+    })
 
     return (
         <main>
@@ -28,36 +113,109 @@ const App = () => {
                     <input
                         type="checkbox"
                         id="mouse-distance"
+                        value={mouseDistanceEnabled}
+                        onClick={() => {
+                            toggleMouseDistance(!mouseDistanceEnabled)
+                            toggleListeners()
+                        }}
                     />
                     <input
                         type="checkbox"
                         id="scroll-wheel-distance"
+                        value={scrollWheelDistanceEnabled}
+                        onClick={() => {
+                            toggleScrollWheelDistance(
+                                !scrollWheelDistanceEnabled
+                            )
+                            toggleListeners()
+                        }}
                     />
 
                     <label htmlFor="mouse-speed"> Speed </label>
                     <input
                         type="checkbox"
                         id="mouse-speed"
+                        value={mouseSpeedEnabled}
+                        onClick={() => {
+                            toggleMouseSpeed(!mouseSpeedEnabled)
+                            toggleListeners()
+                        }}
                     />
                     <input
                         type="checkbox"
                         id="scroll-wheel-speed"
+                        value={scrollWheelSpeedEnabled}
+                        onClick={() => {
+                            toggleScrollWheelSpeed(
+                                !scrollWheelSpeedEnabled
+                            )
+                            toggleListeners()
+                        }}
                     />
                 </article>
                 <article className="mouse-information-container">
-                    <p>
-                        Check/uncheck the checkboxes,
-                        and see how the figure reacts.
+                    {(mouseDistanceEnabled)
+                    ? <p>
+                        Mouse distance: {
+                            mouseDistance.x + mouseDistance.y
+                        }
                     </p>
-                    <button>
+                    : <p>
+                        Mouse distance is not in use.
+                    </p>
+                    }
+
+                    {(scrollWheelDistanceEnabled)
+                    ? <p>
+                        Scroll wheel distance: {scrollWheelDistance}
+                    </p>
+                    : <p>
+                        Scroll wheel distance is not in use.
+                    </p>
+                    }
+
+                    {(mouseSpeedEnabled)
+                    ? <p>
+                        Mouse velocity: {mouseSpeed}
+                    </p>
+                    : <p>
+                        Mouse velocity is not in use.
+                    </p>
+                    }
+
+                    {(scrollWheelSpeedEnabled)
+                    ? <p>
+                        Scroll wheel velocity: {scrollWheelSpeed}
+                    </p>
+                    : <p>
+                        Scroll wheel velocity is not in use.
+                    </p>
+                    }
+
+                    <button onClick={() => {
+                        setMouseSpeed(0)
+                        setMouseDistance({
+                            x: 0,
+                            y: 0
+                        })
+                        setScrollWheelSpeed(0)
+                        setScrollWheelDistance(0)
+                        setPreviousCoordinates(null)
+                        toggleTimer(false)
+                        setTimeElapsed(1)
+                    }}>
                         Reset distances
                     </button>
                 </article>
             </section>
             <figure className="mouse-event-figure">
-                <canvas className="mouse-event-canvas"></canvas>
+                <canvas
+                    className="mouse-event-canvas"
+                    onMouseMove={onMouseMove}
+                    onWheel={onWheel}
+                ></canvas>
                 <figcaption>
-                    See mouse events above
+                    Use the canvas above for the mouse events
                 </figcaption>
             </figure>
         </main>
