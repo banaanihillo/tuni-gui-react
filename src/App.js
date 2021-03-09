22cf693b273d01f0e38e2ad33ae55c7a100a43c7
vjhmur@utu.fi <vjhmur@utu.fi>
2021-03-09 17:31:08 +0200

    Added a color picker
    Exercise 2-1, small:
    Added a radio button for color selection,
    including a primary and secondary color,
    and a color picker for a custom color

--- src/App.js
+++ src/App.js
@@ -1,223 +1,72 @@
-import React, {useState, useEffect} from "react"
+import React, {useState} from "react"
 import "./App.css"
 
 const App = () => {
-    const [mouseDistanceEnabled, toggleMouseDistance] = useState(false)
-    const [
-        scrollWheelDistanceEnabled,
-        toggleScrollWheelDistance
-    ] = useState(false)
-    const [mouseSpeedEnabled, toggleMouseSpeed] = useState(false)
-    const [
-        scrollWheelSpeedEnabled,
-        toggleScrollWheelSpeed
-    ] = useState(false)
-    const [mouseDistance, setMouseDistance] = useState({
-        x: 0,
-        y: 0
-    })
-    const [scrollWheelDistance, setScrollWheelDistance] = useState(0)
-    const [mouseSpeed, setMouseSpeed] = useState(0)
-    const [scrollWheelSpeed, setScrollWheelSpeed] = useState(0)
-    const [
-        previousCoordinates,
-        setPreviousCoordinates
-    ] = useState(null)
-    const [timeElapsed, setTimeElapsed] = useState(1)
-    const [timerOn, toggleTimer] = useState(true)
+    const [color, setColor] = useState("hotpink")
+    const [colorPickerOn, toggleColorPicker] = useState(false)
 
-    useEffect(() => {
-        if (timerOn) {
-            const timeoutIdentifier = setTimeout(() => {
-                setTimeElapsed(timeElapsed + 1)
-            }, 1000)
-            return () => {
-                clearTimeout(timeoutIdentifier)
-            }
-        }
-    },
-    [timeElapsed, timerOn])
-
-    const onMouseMove = (event) => {
-        toggleTimer(true)
-        if (previousCoordinates) {
-            setMouseDistance({
-                x: mouseDistance.x + Math.abs(
-                    previousCoordinates.x
-                    - event.clientX
-                ),
-                y: mouseDistance.y + Math.abs(
-                    previousCoordinates.y
-                    - event.clientY
-                )
-            })
-            setMouseSpeed(Math.floor(
-                (mouseDistance.x + mouseDistance.y)
-                / timeElapsed
-            ))
-        }
-        setPreviousCoordinates({
-            x: event.clientX,
-            y: event.clientY
-        })
+    const changeColor = (event) => {
+        setColor(event.target.value)
+        toggleColorPicker(false)
     }
-
-    const onWheel = (event) => {
-        setScrollWheelDistance((prevState) => {
-            return (prevState + Math.abs(event.deltaY))
-        })
-        setScrollWheelSpeed(Math.floor(
-            scrollWheelDistance / timeElapsed
-        ))
-    }
-
-    /*
-    The specification separates the listeners into four,
-    however, it does seem much more logical to group them together,
-    since velocity is directly related to distance,
-    essentially leading to two listeners - mousemove and wheel,
-    which are removed only when both speed and distance are disabled
-    */
-    const toggleListeners = (() => {
-        const canvasElement = document.querySelector("canvas")
-        if (!mouseDistanceEnabled && !mouseSpeedEnabled) {
-            canvasElement.removeEventListener("mousemove", onMouseMove)
-        }
-        if (!scrollWheelDistanceEnabled && !scrollWheelSpeedEnabled) {
-            canvasElement.removeEventListener("wheel", onWheel)
-        }
-    })
-
+    //
     return (
         <main>
-            <section className="mouse-container">
-                <article className="mouse-checkbox-container">
-                    {/*
-                    The labels in the example look kinda funky,
-                    almost makes one think of a table,
-                    which is not really the most preferable way -
-                    it would be better in terms of user experience,
-                    to instead give each checkbox a unique label,
-                    for easier access to the input;
-                    screen readers follow them consistently,
-                    and mouse/touch users have a larger area to target
-                    */}
-                    <label className="grid-label">
-                        <b> Events </b>
+            <h1 style={{color: color}}> Colo[u]r </h1>
+            <section>
+                <fieldset>
+                    <legend>
+                        Choose a colo[u]r
+                    </legend>
+                    <input
+                        type="radio"
+                        id="primary-color"
+                        value="deeppink"
+                        name="color"
+                        onClick={changeColor}
+                    />
+                    <label htmlFor="primary-color">
+                        Primary colo[u]r
                     </label>
-
-                    <label> Mouse </label>
-                    <label> Scroll wheel </label>
-                    <label htmlFor="mouse-distance"> Distance </label>
-
+                    <br />
                     <input
-                        type="checkbox"
-                        id="mouse-distance"
-                        value={mouseDistanceEnabled}
-                        onClick={() => {
-                            toggleMouseDistance(!mouseDistanceEnabled)
-                            toggleListeners()
+                        type="radio"
+                        id="secondary-color"
+                        value="magenta"
+                        name="color"
+                        onClick={changeColor}
+                    />
+                    <label htmlFor="secondary-color">
+                        Secondary colo[u]r
+                    </label>
+                    <br />
+                    <input
+                        type="radio"
+                        id="custom-color"
+                        name="color"
+                        value="cyan"
+                        onClick={(event) => {
+                            setColor(event.target.value)
+                            toggleColorPicker(true)
                         }}
                     />
-                    <input
-                        type="checkbox"
-                        id="scroll-wheel-distance"
-                        value={scrollWheelDistanceEnabled}
-                        onClick={() => {
-                            toggleScrollWheelDistance(
-                                !scrollWheelDistanceEnabled
-                            )
-                            toggleListeners()
-                        }}
-                    />
-
-                    <label htmlFor="mouse-speed"> Speed </label>
-                    <input
-                        type="checkbox"
-                        id="mouse-speed"
-                        value={mouseSpeedEnabled}
-                        onClick={() => {
-                            toggleMouseSpeed(!mouseSpeedEnabled)
-                            toggleListeners()
-                        }}
-                    />
-                    <input
-                        type="checkbox"
-                        id="scroll-wheel-speed"
-                        value={scrollWheelSpeedEnabled}
-                        onClick={() => {
-                            toggleScrollWheelSpeed(
-                                !scrollWheelSpeedEnabled
-                            )
-                            toggleListeners()
-                        }}
-                    />
-                </article>
-                <article className="mouse-information-container">
-                    {(mouseDistanceEnabled)
-                    ? <p>
-                        Mouse distance: {
-                            mouseDistance.x + mouseDistance.y
-                        }
-                    </p>
-                    : <p>
-                        Mouse distance is not in use.
-                    </p>
-                    }
-
-                    {(scrollWheelDistanceEnabled)
-                    ? <p>
-                        Scroll wheel distance: {scrollWheelDistance}
-                    </p>
-                    : <p>
-                        Scroll wheel distance is not in use.
-                    </p>
-                    }
-
-                    {(mouseSpeedEnabled)
-                    ? <p>
-                        Mouse velocity: {mouseSpeed}
-                    </p>
-                    : <p>
-                        Mouse velocity is not in use.
-                    </p>
-                    }
-
-                    {(scrollWheelSpeedEnabled)
-                    ? <p>
-                        Scroll wheel velocity: {scrollWheelSpeed}
-                    </p>
-                    : <p>
-                        Scroll wheel velocity is not in use.
-                    </p>
-                    }
-
-                    <button onClick={() => {
-                        setMouseSpeed(0)
-                        setMouseDistance({
-                            x: 0,
-                            y: 0
-                        })
-                        setScrollWheelSpeed(0)
-                        setScrollWheelDistance(0)
-                        setPreviousCoordinates(null)
-                        toggleTimer(false)
-                        setTimeElapsed(1)
-                    }}>
-                        Reset distances
-                    </button>
-                </article>
+                    <label htmlFor="custom-color">
+                        Custom colo[u]r
+                    </label>
+                    {colorPickerOn
+                        &&
+                        <input
+                            type="color"
+                            id="custom-color"
+                            value="#00FFFF"
+                            onInput={(event) => {
+                                setColor(event.target.value)
+                            }}
+                        />
+                    } {/* */}
+                </fieldset>
             </section>
-            <figure className="mouse-event-figure">
-                <canvas
-                    className="mouse-event-canvas"
-                    onMouseMove={onMouseMove}
-                    onWheel={onWheel}
-                ></canvas>
-                <figcaption>
-                    Use the canvas above for the mouse events
-                </figcaption>
-            </figure>
+            {/* */}
         </main>
     )
 }
