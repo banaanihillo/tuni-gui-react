5e98d0766b4410b7a0f1f5e44a8a2804a05a014e
vjhmur@utu.fi <vjhmur@utu.fi>
2021-03-10 14:49:58 +0200

    Added exercise 2-3-small (Dialog input)
    Exercise 2-3, small:
    Added text input into the dialog,
    to modify the main paragraph text,
    also changed the alert into a semi-shim,
    to allow for input on unsupported browsers

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from "react"
+import React, {useState} from "react"
 import "./App.css"
 
 const App = () => {
+    const [updatedText, setUpdatedText] = useState("")
+
     const showDialog = (dialogType) => {
-        if (dialogType === "Edit") {
-            let mainText = document.querySelector("#main-text")
-            mainText.innerHTML = "Text updated successfully."
+        if (dialogType !== "Edit") {
+            window.alert(
+                `${dialogType}
+                Click on Edit to modify the main text.
+                `
+            )
         } else {
+            setUpdatedText("")
             let dialogElement = document.querySelector("dialog")
             /*
-            Gecko does not have the dialog element enabled by default,
-            so fall back to an alert element for those user agents
+            Instead of showModal() and close(),
+            set/remove the open attribute on unsupported browsers
             */
             if (typeof dialogElement.showModal === "function") {
                 dialogElement.showModal()
-                let dialogHeader = document.querySelector(
-                    "#dialog-header"
-                )
-                dialogHeader.innerHTML = dialogType
-            } else {
-                window.alert(`${dialogType}
-I am a dialog, sort of
-                `)
+            } else { // Does basically the same thing as showModal()
+                dialogElement.setAttribute("open", "true")
             }
         }
     }
@@ -54,22 +54,42 @@ I am a dialog, sort of
             <main>
                 <h1> Main heading </h1>
                 <section>
-                    <dialog open={false}>
-                        <header id="dialog-header"></header>
+                    <dialog>
+                        <header> Update </header>
                         <p>
-                            I am a dialog
+                            <label htmlFor="updated-text-input">
+                                Update the paragraph text:
+                            </label>
+                            <input
+                                type="text"
+                                id="updated-text-input"
+                                value={updatedText}
+                                onChange={(event) => {
+                                    setUpdatedText(event.target.value)
+                                }}
+                            />
+                            <button onClick={() => {
+                                let dialogElement = document
+                                    .querySelector("dialog")
+                                if (
+                                    typeof dialogElement.close
+                                    === "function"
+                                ) {
+                                    dialogElement.close()
+                                } else {
+                                    dialogElement
+                                        .removeAttribute("open")
+                                }
+                            }}>
+                                OK
+                            </button>
                         </p>
-                        <button onClick={() => {
-                            let dialogElement = document.querySelector(
-                                "dialog"
-                            )
-                            dialogElement.close()
-                        }}>
-                            OK
-                        </button>
                     </dialog>
                     <p id="main-text">
-                        Click on <em> About </em> to show a dialog.
+                        {
+                            updatedText
+                            || "Click on Edit to update."
+                        }
                     </p>
                 </section>
             </main>
