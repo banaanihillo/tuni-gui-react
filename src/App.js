38e05e0321a8234d1de573a125593d391d493667
vjhmur@utu.fi <vjhmur@utu.fi>
2021-03-18 14:11:48 +0200

    Added exercise 3-2-small (Date picker)
    Exercise 3-2-small:
    Added number input for year,
    and select input for month and date,
    to put together a YYYY-MM-DD date,
    since input type="date" forces locale

--- src/App.js
+++ src/App.js
@@ -1,55 +1,174 @@
 import React, {useState, useEffect} from "react"
 import "./App.css"
 
+/*
+    <input type="date"> remains a locale nightmare, so,
+    instead of forcing the user to conform to MM/DD/YYY (for example),
+    use three separate input elements,
+    to make sure the only unambiguous format (YYYY-MM-DD) is used
+*/
+
 const App = () => {
-    const [red, setRed] = useState(0)
-    const [green, setGreen] = useState(0)
-    const [blue, setBlue] = useState(0)
+    const [dates, setDates] = useState([])
+    const [selected, setSelected] = useState(null)
+
     useEffect(() => {
-        let rangeContainer = document.querySelector(".range-container")
-        rangeContainer.style
-            .backgroundColor = `rgb(${red}, ${green}, ${blue})`
+        if (selected) {
+            return () => {
+                selected.classList.remove("selected")
+            }
+        }
     },
-    [red, green, blue])
-    
-    return <div className="range-container">
-        <label htmlFor="red-input">
-            Red
-        </label>
-        <input
-            type="range"
-            min={0}
-            max={255}
-            /* Only for Gecko, css handles the rest */
-            orient="vertical"
-            id="red-input"
-            value={red}
-            onChange={(event) => setRed(event.target.value)}
-        />
-        <label htmlFor="green-input">
-            Green
-        </label>
-        <input
-            type="range"
-            min={0}
-            max={255}
-            orient="vertical"
-            id="green-input"
-            value={green}
-            onChange={(event) => setGreen(event.target.value)}
-        />
-        <label htmlFor="blue-input">
-            Blue
-        </label>
-        <input
-            type="range"
-            min={0}
-            max={255}
-            orient="vertical"
-            id="blue-input"
-            value={blue}
-            onChange={(event) => setBlue(event.target.value)}
-        />
+    [selected])
+
+    const mapOptions = (optionValue) => {
+        return <option key={optionValue}>
+            {/*
+                Pad the months and days from 1 to 9,
+                so they become 01 to 09
+            */}
+            {String(optionValue).padStart(2, "0")}
+        </option>
+    }
+
+    const updateProperty = (dateObject, property, value) => {
+        const updatedDates = dates.map(date => {
+            return (date.id === dateObject.id)
+                ? {...date, [property]: value}
+                : date
+        })
+        setDates(updatedDates)
+    }
+
+    return <div className="application-container">
+        <ul>
+            {dates.map((date, index) => {
+                return <li
+                    key={index}
+                    className="list-container"
+                    onClick={(event) => {
+                        event.currentTarget.classList.add("selected")
+                        setSelected(event.currentTarget)
+                    }}
+                >
+                    {/*    YYYY   -      MM     -     DD */}
+                    {`${date.year}-${date.month}-${date.day}`}
+                    <span>
+                        <label htmlFor={`year-input-${index}`}>
+                            Year (YYYY)
+                        </label>
+                        <input
+                            type="number"
+                            min={1970}
+                            max={9999}
+                            id={`year-input-${index}`}
+                            placeholder="YYYY"
+                            onChange={(event) => {
+                                // "Prevent" years larger than 9999
+                                if (event.target.value.length > 4) {
+                                    return
+                                }
+                                updateProperty(
+                                    date,
+                                    "year",
+                                    /*
+                                    If the input is less than 1000,
+                                    add leading zeroes to make it YYYY
+                                    */
+                                    event.target.value.padStart(4, "0")
+                                )
+                            }}
+                        />
+                    </span>
+                    <span>
+                        <label htmlFor={`month-input-${index}`}>
+                            Month
+                        </label>
+                        <select
+                            onChange={(event) => {
+                                updateProperty(
+                                    date,
+                                    "month",
+                                    event.target.value
+                                )
+                            }}
+                            defaultValue="MM"
+                        >
+                            {/* Use MM as a placeholder, kind of */}
+                            <option disabled>
+                                MM
+                            </option>
+                            {/* 12 months, starting from 01 */}
+                            {[...Array(12)].map((_value, index) => {
+                                return mapOptions(index + 1)
+                            })}
+                        </select>
+                    </span>
+                    <span>
+                        <label htmlFor={`day-input-${index}`}>
+                            Day
+                        </label>
+                        <select
+                            onChange={(event) => {
+                                updateProperty(
+                                    date,
+                                    "day",
+                                    event.target.value
+                                )
+                            }}
+                            defaultValue="DD"
+                        >
+                            <option disabled>
+                                DD
+                            </option>
+                            {/* 31 days, starting from 01 */}
+                            {[...Array(31)].map((_value, index) => {
+                                return mapOptions(index + 1)
+                            })}
+                        </select>
+                    </span>
+                </li>
+            })}
+        </ul>
+        <div className="button-container">
+            <button onClick={() => {
+                const today = new Date()
+                setDates([
+                    ...dates,
+                    {
+                        id: today.toISOString(),
+                        /*
+                        Use a String for year as well,
+                        just for consistency,
+                        although the year will be >= 1970 anyway
+                        */
+                        year: String(today.getFullYear())
+                            .padStart(4, "0"),
+                        /*
+                        Counts from 0 to 11 for some reason,
+                        so just add one,
+                        also pad the default month and day as well,
+                        to modify 1 to 9 into 01 to 09 
+                        */
+                        month: String(today.getMonth() + 1)
+                            .padStart(2, "0"),
+                        day: String(today.getDate())
+                            .padStart(2, "0")
+                    }
+                ])
+            }}>
+                Add
+            </button>
+            <button onClick={() => {
+                if (!selected) {
+                    return console.log("Select an item to remove.")
+                }
+                selected.remove()
+                setSelected(null)
+            }}>
+                Remove
+            </button>
+        </div>
     </div>
 }
 
