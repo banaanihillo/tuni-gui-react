b400ab9b9645881b282de8e286cbe217d54c8c34
vjhmur@utu.fi <vjhmur@utu.fi>
2021-03-25 17:25:29 +0200

    Added undo and redo things (4-3-small)
    Exercise 4-3-small:
    Added three buttons for text addition,
    and a paragraph displaying the text,
    also added undo and redo buttons,
    making use of arrays as "stacks"

--- src/App.js
+++ src/App.js
@@ -1,55 +1,90 @@
 import React, {useState} from "react"
 import "./App.css"
-// Public domain, via pikist.com
-import torontoSkyline from "./toronto-skyline-500x330.jpg"
-// Also public domain, via photos-public-domain.com
-import orangeSunset from "./orange-sunset-with-trees-600x400.jpg"
+//
 
 const App = () => {
-    const [image, setImage] = useState(torontoSkyline)
-    return <main>
-        <img
-            src={image}
-            width={500}
-            height={330}
-            alt="The skyline of Toronto, with Rogers Centre and the CN Tower in the forefront"
-            onDrop={(event) => {
-                event.preventDefault()
-                let file = event.dataTransfer.files[0]
-                if (file && file.type === "image/jpeg") {
-                    let fileReader = new FileReader()
-                    fileReader.onloadend = () => {
-                        setImage(fileReader.result)
-                    }
-                    fileReader.readAsDataURL(file)
-                }
-            }}
-            onDragOver={(event) => {
-                event.preventDefault()
-                event.dataTransfer.dropEffect = "copy"
-            }}
-        />
-        <hr /> 
-        <figure>
-            <figcaption>
-                Try dragging this image from the source folder,
-                or any jpeg image, really, <br />
-                onto the existing Toronto skyline image
-            </figcaption>
-            <img
-                src={orangeSunset}
-                alt="Dark outlines of trees showered by the light of the setting Sun"
-            />
-            <p>
-                As for the above image itself - <br />
-                It is purely decorational,
-                just acting as a reminder that you may freely test the functionality with the existing image in the source folder
-            </p>
-            <footer>
-                The images used are in the public domain.
-            </footer>
-        </figure>
-    </main> 
+    const [text, setText] = useState("")
+    const [undoStack, setUndoStack] = useState([])
+    const [redoStack, setRedoStack] = useState([])
+
+    const appendText = (event) => {
+        setText(text + event.target.innerText)
+        setUndoStack([
+            ...undoStack,
+            event.target.innerText
+        ])
+    }
+
+    const undoText = () => {
+        // Do nothing if the undo stack is empty
+        if (undoStack.length > 0) {
+            // Make a shallow copy of the current undo stack,
+            const copyOfUndoStack = [...undoStack]
+            // and remove the item on the top of the stack
+            const removedItem = copyOfUndoStack.pop()
+            // Set the text as the old text minus the removed item
+            setText(copyOfUndoStack.join(""))
+            // Update the undo stack,
+            // to no longer include the undoed (undid?) item
+            setUndoStack(copyOfUndoStack)
+            // Add the removed undo item into the redo stack
+            setRedoStack([
+                ...redoStack,
+                removedItem
+            ])
+        }
+    }
+
+    const redoText = () => {
+        // Also do nothing if the redo stack is empty
+        if (redoStack.length > 0) {
+            // Take the item on top of the stack,
+            const lastItem = redoStack[redoStack.length - 1]
+            // and append it onto the existing text
+            setText(text + lastItem)
+            // Make a shallow copy of the current redo stack,
+            const copyOfRedoStack = [...redoStack]
+            // and remove the item on top of the stack
+            copyOfRedoStack.pop()
+            // Update the redo stack,
+            // to no longer include the redone (?) item
+            setRedoStack(copyOfRedoStack)
+            // Add the removed redo item into the undo stack
+            setUndoStack([
+                ...undoStack,
+                lastItem
+            ])
+        }
+    }
+
+    return (
+        <main>
+            <section>
+                <p>
+                    {text}
+                </p>
+            </section>
+            <section>
+                <button onClick={appendText}>
+                    banana
+                </button>
+                <button onClick={appendText}>
+                    ananas
+                </button>
+                <button onClick={appendText}>
+                    bananas
+                </button>
+            </section>
+            <section>
+                <button onClick={undoText}>
+                    Undo
+                </button>
+                <button onClick={redoText}>
+                    Redo
+                </button>
+            </section>
+        </main>
+    )
 }
 
 export default App
