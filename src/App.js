5fd76a3a79bdcac10d73c469bd2e49f2b25aa7a3
vjhmur@utu.fi <vjhmur@utu.fi>
2021-03-30 15:17:12 +0300

    Added paintable houses (5-1-small)
    Exercise 5-1-small:
    Added a canvas element,
    roughly the size of the user's window,
    also added a button for symmetric houses,
    painting a 50 by 50 house at a random point,
    also added input for custom sizes,
    painting wacky houses at random points,
    also added a button for clearing the canvas

--- src/App.js
+++ src/App.js
@@ -1,127 +1,145 @@
-import React, {useState} from "react"
+import React, {
+    useState,
+    useEffect,
+    useRef as useReference
+} from "react"
 import "./App.css"
-import torontoSkyline from "./toronto-skyline-500x330.jpg"
-
+//
 const App = () => {
-    const [image, setImage] = useState(torontoSkyline)
-    const [headingText, setHeadingText] = useState("Editable heading")
-    const [captionText, setCaptionText] = useState("Editable caption")
-    const [undoStack, setUndoStack] = useState(null)
-    const [redoStack, setRedoStack] = useState(null)
+    // Let the canvas breathe just a bit,
+    // to avoid a one-pixel overflow and scrollbars
+    const [canvasWidth, setCanvasWidth] = useState(
+        window.innerWidth - 20
+    )
+    // Also make some space for the buttons
+    const [canvasHeight, setCanvasHeight] = useState(
+        window.innerHeight - 100
+    )
+    const [houseWidth, setHouseWidth] = useState(10)
+    const [houseHeight, setHouseHeight] = useState(10)
+    const canvasReference = useReference(null)
+
+    useEffect(() => {
+        // Re-set the canvas width and height,
+        // whenever the user modifies the window size
+        window.onresize = (event) => {
+            setCanvasWidth(event.target.innerWidth - 20)
+            setCanvasHeight(event.target.innerHeight - 100)
+        }
+    },
+    [canvasReference])
+
+    const paintHouse = (width, height) => {
+        // Place the house at a random point,
+        // between (0, 0) and (canvasWidth, canvasHeight)
+        const randomX = Math.round(
+            Math.random() * canvasWidth
+        )
+        const randomY = Math.round(
+            Math.random() * canvasHeight
+        )
+        let context = canvasReference.current.getContext("2d")
+        context.strokeStyle = "magenta"
+        context.fillStyle = "magenta"
+        context.fillRect(
+            randomX,
+            randomY,
+            width,
+            height
+        )
+        let path = new Path2D()
+        // Paint the roof from the top-left corner of the rectangle,
+        path.moveTo(randomX, randomY)
+        // turning around at half the width and height,
+        path.lineTo(
+            randomX + (width / 2),
+            randomY - (height / 2)
+        )
+        // and finishing at the full width of the rectangle,
+        // and at the same height the roof started from
+        path.lineTo(
+            randomX + width,
+            randomY
+        )
+        path.closePath()
+        context.fillStyle = "deeppink"
+        context.fill(path)
+    }
 
     return <span>
-        <main
-            contentEditable={true}
-            suppressContentEditableWarning={true}
-            onCopy={(event) => {
-                event.preventDefault()
-                // Retain the source formatting here,
-                event.clipboardData.setData(
-                    "text/html",
-                    event.target.innerHTML
-                )
-                // and also allow, for instance,
-                // the "Keep Text Only" option on Word
-                event.clipboardData.setData(
-                    "text/plain",
-                    event.target.innerHTML
-                )
-            }}
-            onPaste={(event) => {
-                event.preventDefault()
-                setUndoStack(event.target.outerHTML)
-                const pastedText = event.clipboardData.getData("text")
-                const splitText = pastedText.split("\n")
-                setHeadingText(splitText[0])
-                setCaptionText(splitText[1])
-            }}
-        >
-            <header>
-                <h1>
-                    {headingText}
-                </h1>
-            </header>
-            <figure>
-                <img
-                    src={image}
-                    alt="The roof of Rogers Centre is open, thirty minutes before first pitch"
-                    onDrop={(event) => {
-                        event.preventDefault()
-                        const file = event.dataTransfer.files[0]
-                        if (file && file.type === "image/jpeg") {
-                            const fileReader = new FileReader()
-                            fileReader.onloadend = () => {
-                                setImage(fileReader.result)
-                            }
-                            fileReader.readAsDataURL(file)
-                        }
-                    }}
-                    onDragOver={(event) => {
-                        event.preventDefault()
-                        event.dataTransfer.dropEffect = "copy"
-                    }}
-                />
-                <figcaption>
-                    {captionText}
-                </figcaption>
-            </figure>
-        </main>
-        <footer>
+        <main>
+            <canvas
+                ref={canvasReference}
+                width={canvasWidth}
+                height={canvasHeight}
+            ></canvas>
             <span className="button-container">
                 <button onClick={() => {
-                    if (!undoStack) {
-                        return console.log("Nothing to undo.")
-                    }
-                    const mainElement = document.querySelector("main")
-                    setRedoStack(mainElement.outerHTML)
-                    mainElement.outerHTML = undoStack
-                    setUndoStack(null)
+                    paintHouse(50, 50)
                 }}>
-                    Undo
+                    Add a symmetric house
                 </button>
-                <button onClick={() => {
-                    if (!redoStack) {
-                        return console.log("Nothing to redo.")
-                    }
-                    const mainElement = document.querySelector("main")
-                    setUndoStack(mainElement.outerHTML)
-                    mainElement.outerHTML = redoStack
-                    setRedoStack(null)
+                <form onSubmit={(event) => {
+                    event.preventDefault()
+                    paintHouse(houseWidth, houseHeight)
                 }}>
-                    Redo
+                    <span className="input-container">
+                        <label htmlFor="width-input">
+                            Width
+                        </label>
+                        <input
+                            type="number"
+                            min={10}
+                            max={100}
+                            id="width-input"
+                            value={houseWidth}
+                            title="Width between 10 and 100 pixels"
+                            onChange={(event) => {
+                                setHouseWidth(
+                                    Number(event.target.value)
+                                )
+                            }}
+                        />
+                        <br />
+                        <label htmlFor="height-input">
+                            Height
+                        </label>
+                        <input
+                            type="number"
+                            min={10}
+                            max={100}
+                            id="height-input"
+                            value={houseHeight}
+                            title="Height between 10 and 100 pixels"
+                            onChange={(event) => {
+                                setHouseHeight(
+                                    Number(event.target.value)
+                                )
+                            }}
+                        />
+                    </span>
+                    <button type="submit">
+                        Add a wacky house
+                    </button>
+                </form>
+                <button
+                    type="cancel"
+                    onClick={() => {
+                        let context = canvasReference.current
+                            .getContext("2d")
+                        context.clearRect(
+                            0,
+                            0,
+                            canvasWidth,
+                            canvasHeight
+                        )
+                    }}
+                >
+                    Clear canvas
                 </button>
             </span>
-            <p>
-                Use the context menu for text content manipulation.
-                <br />
-                Click anywhere on the above container to access all the available commands detailed below.
-            </p>
-            <p>
-                Select all (<code>ctrl + a</code>)
-                enables selection of all the page content.
-            </p>
-            <p>
-                Copy (<code>ctrl + c</code>)
-                copies the selection as rich text.
-                Try pasting it into Word, for example.
-                <br />
-                Also try copying text content with a line break in between from somewhere else,
-                and Paste (<code>ctrl + v</code>) it here.
-            </p>
-            <p>
-                After making a change,
-                you can Undo (<code>ctrl + z</code>) that change,
-                or Redo (<code>ctrl + y</code>) a change that you just undid.
-                <br />
-                Some browsers may also use
-                <code> ctrl + shift + z </code>
-                as Redo.
-            </p>
-            <p>
-                If you wish to manipulate text pasted in from other sources,
-                use the provided Undo and Redo buttons.
-            </p>
-        </footer>
+        </main>
+        <footer></footer>
     </span>
 }
 
